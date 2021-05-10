ec4b063ec7a56653fd8dd445321225c9ac582de9
NGAGEZA <44493178+NGAGEZA@users.noreply.github.com>
2021-05-10 16:59:24 +0700

    Add project files.

---
+++ RISTExamOnlineProject/Controllers/AccountController.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RISTExamOnlineProject.Models.db;
+
+namespace RISTExamOnlineProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SPTODbContext _sptoDbContext;
+
+        public AccountController(SPTODbContext context)
+        {
+            _sptoDbContext = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(UserLoginModel userModel, string returnUrl = null)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(userModel);
+        //    }
+
+        //    var result = await _signInManager.PasswordSignInAsync(userModel.Email, userModel.Password, userModel.RememberMe, false);
+        //    if (result.Succeeded)
+        //    {
+        //        return RedirectToLocal(returnUrl);
+        //    }
+        //    else
+        //    {
+        //        ModelState.AddModelError("", "Invalid UserName or Password");
+        //        return View();
+        //    }
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string OperatorID, string Password, vewOperatorAlls model, string returnUrl = null)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if (!string.IsNullOrEmpty(OperatorID) && string.IsNullOrEmpty(Password)) return RedirectToAction("Login");
+
+            //Check the user name and password
+            //Here can be implemented checking logic from the database
+            ClaimsIdentity identity = null;
+            var isAuthenticated = false;
+            string authority = null;
+            var active = false;
+            var querylogin = await _sptoDbContext.vewOperatorAll
+                .Where(x => x.OperatorID == OperatorID && x.Password == Password)
+                .Select(c => new {c.OperatorID, c.Active, c.Authority}).FirstOrDefaultAsync();
+
+
+            if (querylogin != null)
+            {
+                //foreach (var item in querylogin)
+                //{
+                //    authority = item.Authority;
+                //    active = item.Active;
+                //}
+
+                authority = querylogin.Authority;
+                active = querylogin.Active;
+
+                if (active == false) return RedirectToAction("Login");
+
+                switch (authority)
+                {
+                    case "9":
+                        //Create the identity for the Admin
+                        identity = new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.Name, model.OperatorID),
+
+                            new Claim(ClaimTypes.Role, "Admin"),
+                        }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        isAuthenticated = true;
+                        break;
+                    case "7":
+                        //Create the identity for the Training
+                        identity = new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.Name, model.OperatorID),           
+                            new Claim(ClaimTypes.Role, "Training")
+                        }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        isAuthenticated = true;
+                        break;
+                    case "0":
+                        //Create the identity for the user
+                        identity = new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.Name, model.OperatorID),                   
+                            new Claim(ClaimTypes.Role, "User")
+                        }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        isAuthenticated = true;
+                        break;
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid Operator No. or Password");
+                return View();
+            }
+           
+ 
+
+
+                if (!isAuthenticated) return View();
+            var principal = new ClaimsPrincipal(identity);
+
+            var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            return RedirectToLocal(returnUrl);
+            //return RedirectToAction("Index", "Home");
+        
+        }
+
+        public IActionResult Logout()
+        {
+            var login = HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ RISTExamOnlineProject/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RISTExamOnlineProject.Models.db;
+using System.Linq;
+
+namespace RISTExamOnlineProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SPTODbContext _sptoDbContext;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public HomeController(SPTODbContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
+        { 
+            _sptoDbContext = context;
+            _configuration = configuration;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            var UserName = User.Identity.Name;
+            vewOperatorAlls dataOperator = new vewOperatorAlls();
+            dataOperator = _sptoDbContext.vewOperatorAll.FirstOrDefault(x => x.OperatorID == UserName);
+            ViewBag.NameEng = dataOperator.NameEng;
+            ViewBag.JobTitle = dataOperator.JobTitle;
+            var varsd = "http://10.29.1.12/RAJPTrainingControlSystem/PIC/"+ UserName+".jpg";
+
+            ViewBag.imgProfile = varsd;
+            return View();
+        }
+
+        public JsonResult Profile(string OPID)
+        {
+            var UserName = OPID;
+             
+            vewOperatorAlls dataOperator = new vewOperatorAlls();
+            dataOperator = _sptoDbContext.vewOperatorAll.FirstOrDefault(x => x.OperatorID == UserName);
+            var Name = dataOperator.NameEng;
+            var JobTitle = dataOperator.JobTitle;
+            var varsd = "http://10.29.1.12/RAJPTrainingControlSystem/PIC/" + UserName + ".jpg";
+
+            var jsonResult = Json(new{ data=varsd ,strName = Name, strJobTitle = JobTitle } );
+
+            return jsonResult;
+        }
+    }
+}
